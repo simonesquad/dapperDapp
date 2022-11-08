05a306fd982ead9129c3c018df91b31762684c85
Simone Ballard <simone.a.ballard@gmail.com>
2022-11-08 17:02:51 -0600

    changed moralis versions

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import { MoralisProvider } from "react-moralis";
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <MoralisProvider appId="001" serverUrl="http://localhost:1337/server">
     <App />
-  </React.StrictMode>
+  </MoralisProvider>
 );
 
