7c261e188bf0fd65aeaa316e7c914b596e42cf0e
Simone Ballard <simone.a.ballard@gmail.com>
2022-11-08 10:59:48 -0600

    reverted to earlier index.js and verified that the provider error does not currently show up with the backend server running but some ui issues still not fully resolved

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,13 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import { MoralisProvider } from "react-moralis";
+import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
-ReactDOM.render(
-  <MoralisProvider>
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
     <App />
-  </MoralisProvider>,
-  document.getElementById("root"),
+  </React.StrictMode>
 );
 
