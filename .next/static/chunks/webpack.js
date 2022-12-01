5006383b27acd6ad590e9c29a1e71bdde533399e
Simone Ballard <simone.a.ballard@gmail.com>
2022-11-30 19:17:00 -0600

    need to figure out this spacing tomorrow

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -136,7 +136,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "93de7fe0c5be2c95"; }
+/******/ 		__webpack_require__.h = function() { return "7476643a1dd9d050"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/components/navbar.js
+++ pages/components/navbar.js
@@ -5,23 +5,18 @@ function Navbar() {
     return (
         <>
             <nav class="navbar navbar-expand-xl bg-info">
-                <div class="d-flex mb-2">
-                    <div class="me-auto p-2">
+                <div class="d-flex justify-content-between">
                         <a class="navbar-brand" href="/">
-                        {/* <Image src="/demo-logo.png" alt="Logo" width="30" height="24" /> */}
                         <span class="navbar-text">
                             Dappy!
                         </span>
                         </a> 
-                    </div>
-                    <div class="p-2">
                         <button 
                             class="btn btn-sm btn-outline-secondary"
                             type="button"
                             onClick={() => signOut({ redirect: '/signin' })}>
                                 Logout
                         </button>
-                    </div>
                 </div>
             </nav> 
         </>
